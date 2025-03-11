3c393a3fbc3a0f55fd0848cc860c6628f6ba10c6
santiago.vergara.7e8 <santiago.vergara.7e8@itb.cat>
2025-03-11 10:41:24 +0100

    First Commit

---
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Navigation/Navigation.kt
@@ -0,0 +1,30 @@
+package cat.itb.m78.exercices.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import cat.itb.m78.exercices.Counter.Counter
+import kotlinx.serialization.Serializable
+
+object Destination
+{
+    @Serializable
+    data object Screen1
+}
+
+@Composable
+fun Navigation()
+{
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Destination.Screen1
+    )
+    {
+        composable<Destination.Screen1>
+        {
+            Counter()
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Settings/ViewModel.kt
@@ -0,0 +1,18 @@
+package cat.itb.m78.exercices.Settings
+
+import androidx.lifecycle.ViewModel
+import com.russhwolf.settings.Settings
+
+class ViewModelCounter : ViewModel()
+{
+
+
+
+}
+
+class Setts()
+{
+    val settings: Settings = Settings()
+
+    private var visitCounter = 0;
+}
